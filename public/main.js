bf95ded90654d922b0997648ff0492753d404fc3
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-06-13 10:03:42 +0200

    Expand comments

--- public/main.js
+++ public/main.js
@@ -1,28 +1,36 @@
 let deleteButtons = Array.from(document.getElementsByClassName("delete"));
+let likeButtons = Array.from(document.getElementsByClassName("like"));
+
+// Both of these declarations are doing the same basic thing: using getElementsByClassName() to select all of the relevant DOM elements.  getElementsByClassName returns those elements as an HTMLCollection, which isn't quite the same thing as an array.
+// Since we're going to need to iterate through the lists of buttons, we convert the HTMLCollections into arrays using Array.from()
+
 deleteButtons.forEach((button) =>
   button.addEventListener("click", deleteRapper)
 );
-
-let likeButtons = Array.from(document.getElementsByClassName("like"));
 likeButtons.forEach((button) => button.addEventListener("click", addLike));
 
+// Again, the same method is being applied to both arrays:  forEach() iterates through them and attaches an eventListener to each element
+
+
 async function addLike() {
-  const sName = this.parentNode.childNodes[1].innerText;
+  const sName = this.parentNode.childNodes[1].innerText;  // "this" refers to the button that was pressed. What's happening here is that the code is walking up its family tree to find the parentNode (in this case, the list element that contains the button).  Then it looks back down the tree to find a particular childNode and selects that the childNode contains.  That is the value that's being assigned to "sName"
   try {
-    const response = await fetch("addLike", {
-      method: "put",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ 
-        "stageNameS": sName 
+    const response = await fetch("addLike", {  // Here we're building our fetch request.  It starts with the endpoint and continues with the contents of the request object:  basically, the information that is going to be sent to the server.
+      method: "put",  // The server needs to know what we're trying to do
+      headers: { "Content-Type": "application/json" },  // The server also needs to know what format the data is coming in (this is VERY IMPORTANT)
+      body: JSON.stringify({  // And here's where the body of the request is built: JSON.stringify() takes the object and turns it into a JSON
+        "stageNameS": sName
       }),
     });
-    const data = await response.json();
-    console.log(data);
-    location.reload();
+    const data = await response.json(); // response.json() on the client-side does the OPPOSITE of its server-side twin:  here it's taking a JSON input and parsing it to produce a JavaScript object (which will be assigned to the variable name "data")
+    console.log(data);  // The JavaScript object is logged to the console..
+    location.reload(); // And the page is refreshed (making another "GET" request to the server)
   } catch (error) {
     console.log(error);
   }
 }
+
+// And everything that's happening here is exactly the same as what happened above.  Only the method and endpoint differ.
 
 async function deleteRapper() {
   const sName = this.parentNode.childNodes[1].innerText;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express();  // the standard way to set up express
+const app = express();  // these first two lines are the standard way to set up express
 const PORT = 5000;
 const MongoClient = require("mongodb").MongoClient; // sets up how we talk to MongoDB
 require("dotenv").config(); // preps the environmental variables
@@ -9,7 +9,7 @@ let db,
   dbConnectionStr = process.env.DB_STRING, // accesses the environmental variable
   dbName = "rap";
 
-  // set up the connection to the database
+// set up the connection to the database
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
   (client) => {
     console.log(`Connected to ${dbName} Database`);
@@ -17,17 +17,20 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
   }
 );
 
-app.set("view engine", "ejs");  // lets express know that we're using ejs as a templating engine
-app.use(express.static("public")); // thanks to this line, we don't have to write custom routes to serve static files (express will serve them automatically)
+app.set("view engine", "ejs");  // lets Express know that we're using ejs as a templating engine
+app.use(express.static("public")); // thanks to this line, we don't have to write custom routes to serve static files (Express will serve them automatically)
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // these two lines replace allow us to access data on the request body that is formatted as a JSON or URL-encoded (e.g., form data)
+app.use(express.urlencoded({ extended: true })); // these two lines allow us to access data on the request body that is formatted as a JSON or URL-encoded (e.g., form data)
 
-app.get("/", (request, response) => {
+
+// Here is where we start telling the server what to listen for (the method), where to listen (the endpoint), and what to do when a request comes through (the function)
+
+app.get("/", (request, response) => {  // Within this function we are defining what happens when the server hears this request
   db.collection("rappers").find().toArray()
-    // What this is saying is that when a GET request is made, the server will go to the database, access the collection named "rappers," find all of the documents, and return that list of documents into an array of objects
-    .then((data) => {
+    // What this is saying is that when a GET request is made to the root directory ("/"), the server will go to the database, access the collection named "rappers," find all of the documents, convert them into an array, and then return that array
+    .then((data) => {  // "data" here refers to the array that was just returned, i.e. the array of documents from the "rappers" collection
       response.render("index.ejs", { info: data });
-      // The server then prepares the response to the client.  It's going to send the index.ejs file, rendered into HTML.  But in order to render the file, EJS needs an object with the information that it can use to "fill in the blanks."  
+      // The server then prepares the response to the client.  It's going to send the index.ejs file, rendered into HTML.  But in order to render the file, it needs to send EJS an object containing the information that it can use to "fill in the blanks."  
       // In this case, the server is sending EJS an object with the property "info" and the value "data" -- i.e., the array of rappers taken from the database
       // This is why when you look in index.ejs you'll see references to "info"
     })
@@ -35,18 +38,18 @@ app.get("/", (request, response) => {
 });
 
 app.post("/addRapper", (request, response) => {
-  // The server is set up to listen for "POST" requests made to the "/addRapper" endpoint.
+  // The server is set up to listen for POST requests made to the "/addRapper" endpoint.
   db.collection("rappers")
-  // When a request comes in, the server accesses the collection named "rappers" on the database
+    // When a request comes in, the server once again accesses the collection named "rappers" on the database
     .insertOne({
       stageName: request.body.stageName,
       birthName: request.body.birthName,
       likes: 0,
     })
-    // A new document is created, using the insertOne() method and information taken from the body of the request
+    // Instead of finding data, it uses the insertOne() method and information taken from the body of the request to make a new entry
     .then((result) => {
       console.log("Rapper added");  // The server logs this message
-      response.redirect("/"); // This returns the browser to the root index; in effect it forces a page refresh (which as we now know, means a "GET" request
+      response.redirect("/"); // This returns the browser to the root index; in effect it forces a page refresh (which, as we now know, means a "GET" request)
     })
     .catch((error) => console.error(error));
 });
@@ -60,7 +63,7 @@ app.put("/addLike", (request, response) => {
     // Using the updateOne() method, a document is located that matches the given key:value pair, and the value of its "likes" property is increased by one 
     .then((result) => {
       console.log(result);
-      response.json("Likes updated");  // The server converts this string to a JSON and sends it to the client.  Check main.js to see what the client does with it.
+      response.json("Likes updated");  // response.json() in Express accepts an object or array (or, in this case, a string), converts it to a JSON, and sends it to the client.  Check main.js to see what the client does with it.
     })
     .catch((error) => console.error(error));
 });
